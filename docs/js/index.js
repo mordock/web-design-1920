e88504ed5cc4bc9e1ba263c5d6fec7876019c915
DESKTOP-2CCSS00\Max <maxweijers@hotmail.nl>
2020-04-15 11:17:17 +0200

    push for publish

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,14 +1,16 @@
 let button = document.getElementById('nextButton');
-let backButton = document.getElementById('TerugButton2');
-let table1 = document.getElementById('table1');
-let table2 = document.getElementById('table2');
+let button2 = document.getElementById('nextButton2');
+let backButton = document.getElementById('backButton2');
+let table1 = document.getElementById('table1Box');
+let table2 = document.getElementById('table2Box');
+let table3 = document.getElementById('table3Box');
 
 let body = document.getElementsByTagName('body');
 
 button.addEventListener('click', function(){
     table1.classList.add('hidden');
     table2.classList.remove('hidden');
-    document.body.style.background = 'grey';
+    document.body.style.background = 'lightgrey';
 });
 
 backButton.addEventListener('click', function(){
@@ -16,4 +18,10 @@ backButton.addEventListener('click', function(){
     table2.classList.add('hidden');
     document.body.style.background = 'black';
 
+});
+
+button2.addEventListener('click', function(){
+    table2.classList.add('hidden');
+    table3.classList.remove('hidden');
+    document.body.style.background = 'black';
 });
