ca1cb3b5c50b6a4ed3c695677b2e797a65564e2f
DESKTOP-2CCSS00\Max <maxweijers@hotmail.nl>
2020-04-28 22:02:55 +0200

    finished I hope

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,25 +1,12 @@
 let body = document.getElementsByTagName('body');
 
+//buttons
 let testButton = document.getElementById('button');
 // let stopB = document.getElementById('stop');
 let nextButton = document.getElementById('next');
 let introButton = findElement('chromeIntro');
-
-//table
-let header = findElement('header');
-let Centrale1 = findElement('C1');
-    Centrale2 = findElement('C2');
-    Centrale3 = findElement('C3');
-    Centrale4 = findElement('C4');
-    Centrale5 = findElement('C5');
-    Centrale6 = findElement('C6');
-    Centrale7 = findElement('C7');
-    Centrale8 = findElement('C8');
-    Centrale9 = findElement('C9');
-    Centrale10 = findElement('C10');
-let sub1 = findElement('sub1');
-    sub2 = findElement('sub2');
-    sub3 = findElement('sub3');
+let repeatButton = findElement('repeatButton');
+let pauseButton = findElement('pauseButton');
 
 //sections
 let beginning = findElement('beginning');
@@ -31,6 +18,9 @@ let synth = window.speechSynthesis;
 let utter = new SpeechSynthesisUtterance();
 
 let lastSpokenText = '';
+
+let range = findElement('rangeSpeed');
+let rangeVolume = findElement('rangeVolume');
 
 //control booleans
 let isOnNext = false;
@@ -46,9 +36,9 @@ function init(){
     utter.pitch = 1;
     utter.rate = .85;
     utter.lang = 'nl-NL'
-    console.log(speechSynthesis.getVoices());
-    speak('Hallo Rogé, vandaag ga we deze gepersonaliseerde site bekijken en bespreken wat je er van vindt. Ik wil je hierbij vragen om hardop na te denken.');
-    speak('Voor deze site kan je je screen reader uitzetten. Je kan de tab toets gebruiken om door de site heen te gaan.')
+    console.log('bob');
+    speak('Hallo Rogé, hoe gaat het met je vandaag? Je kan hier verderop een tabel vinden over wat informatie van kolencentrales in Nederland.');
+    speak('Voor deze site kan je je screen reader uitzetten. Je kan de muis of de tab toets gebruiken om de site te gebruiken.')
 }
 
 window.addEventListener('keydown', function(e){
@@ -88,26 +78,61 @@ window.onload = function(){
     this.setInterval(test, 50);
 }
 
-//event listeners
+// event listeners
 // stopB.addEventListener('focus', function(){
 //     synth.cancel();
-
 // });
+
+range.addEventListener('input', function(){
+    utter.rate = range.value;
+    synth.cancel();
+    let rate = utter.rate * 100;
+    rate = rate.toFixed(0);
+    let speechSpeed = 'spraak snelheid veranderd naar' + rate + 'procent';
+    utter.text = speechSpeed;
+    synth.speak(utter);
+    speak(lastSpokenText);
+});
+
+rangeVolume.addEventListener('input', function(){
+    utter.volume = rangeVolume.value;
+    synth.cancel();
+    let volume = utter.volume * 100;
+    volume = volume.toFixed(0);
+    let speechVolume = 'spraak volume veranderd naar' + volume + 'procent';
+    utter.text = speechVolume;
+    synth.speak(utter);
+    speak(lastSpokenText);
+});
+
+repeatButton.addEventListener('click', function(){
+    speak(lastSpokenText);
+});
+
+pauseButton.addEventListener('click', function(){
+    if(isPaused == false){
+        synth.pause();
+        isPaused = true;
+    }else if(isPaused == true){
+        synth.resume();
+        isPaused = false;
+    }
+});
 
 introButton.addEventListener('click', function(){
     init();
     beginning.classList.remove('hidden');
     nextButton.classList.remove('hidden');
     introButton.classList.add('hidden');
-})
+});
 
 nextButton.addEventListener('click', function(){
     synth.cancel();
     if(controlInt === 1){
         hideAndReveal(beginning, tutorial);
         synth.cancel();
-        speak('Ik ga je nu een snelle uitleg geven over de site. Je kan met de tab toets door de site heen navigeren. Met de shift toets kan je op elk moment de screenreader pauzeren. Door nogmaals op shift te drukken gaat die weer door.');
-        speak('Door op de R toets te drukken kan je het laatste stuk tekst opnieuw luisteren.');
+        speak('Ik ga je nu een snelle uitleg geven over de site. Onderaan de website staat enkele knoppen om te de screen reader te gebruiken. Hiermee kan je de tekst pauzeren en de laatst gesproken tekst herhalen. Pauzeren kan ook met shift en herhalen met de r toets.');
+        speak('Met de vakjes daarnaast kan je het volume en de spraak snelheid van de stem aanpassen als dit nodig is. ');
         controlInt++;
     }else if(controlInt === 2){
         hideAndReveal(tutorial, table);
@@ -133,79 +158,3 @@ function findElement(ID){
     return element;
 }
 
-header.children[0].addEventListener('focusin', () => {speak('Kop, Oude kolencentrales');});
-header.children[1].addEventListener('focusin', () => {speak('Kop, Bouwjaar');});
-header.children[2].addEventListener('focusin', () => {speak('Kop, Vermogen');});
-header.children[3].addEventListener('focusin', () => {speak('Kop, rendement');});
-header.children[4].addEventListener('focusin', () => {speak('Kop, C o 2 uitstoot');});
-
-Centrale1.children[0].addEventListener('focusin', () => {speak('Kolencentrale Delta in Borsele');});
-Centrale1.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1988');});
-Centrale1.children[2].addEventListener('focusin', () => {speak('Vermogen 406 Mega Watt');});
-Centrale1.children[3].addEventListener('focusin', () => {speak('Rendement 38% voor alle oude centrales');});
-Centrale1.children[4].addEventListener('focusin', () => {speak('2.1 Megaton uitstoot per jaar');});
-
-Centrale2.children[0].addEventListener('focusin', () => {speak('Kolencentrale Essent');});
-Centrale2.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1981');});
-Centrale2.children[2].addEventListener('focusin', () => {speak('Vermogen 645 Mega Watt');});
-Centrale2.children[3].addEventListener('focusin', () => {speak('2.1 Megaton uitstoot per jaar');});
-
-Centrale3.children[0].addEventListener('focusin', () => {speak('Kolencentrale Engie in Gelderland');});
-Centrale3.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1985');});
-Centrale3.children[2].addEventListener('focusin', () => {speak('Vermogen 570 Mega Watt');});
-Centrale3.children[3].addEventListener('focusin', () => {speak('2.8 Megaton uitstoot per jaar');});
-
-Centrale4.children[0].addEventListener('focusin', () => {speak('Kolencentrale Uniper in Maasvlakte');});
-Centrale4.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1988');});
-Centrale4.children[2].addEventListener('focusin', () => {speak('Vermogen 500 Mega Watt');});
-Centrale4.children[3].addEventListener('focusin', () => {speak('3.0 Megaton uitstoot per jaar');});
-
-Centrale5.children[0].addEventListener('focusin', () => {speak('Kolencentrale Uniper in Maasvlakte');});
-Centrale5.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1989');});
-Centrale5.children[2].addEventListener('focusin', () => {speak('Vermogen 500 Mega Watt');});
-Centrale5.children[3].addEventListener('focusin', () => {speak('3.0 Megaton uitstoot per jaar');});
-
-sub1.children[0].addEventListener('focusin', () => {speak('Kop, Totaal oude kolencentrales');});
-sub1.children[1].addEventListener('focusin', () => {speak('Kop, leeg');});
-sub1.children[2].addEventListener('focusin', () => {speak('Kop, Totaal vermogen is 2621 voor oude centrales');});
-sub1.children[3].addEventListener('focusin', () => {speak('Kop, Leeg');});
-sub1.children[4].addEventListener('focusin', () => {speak('Kop, Totale uitstoot 13.0 voor oude centrales');});
-
-Centrale6.children[0].addEventListener('focusin', () => {speak('Kolencentrale NUON');});
-Centrale6.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1995');});
-Centrale6.children[2].addEventListener('focusin', () => {speak('Vermogen 630 Mega Watt');});
-Centrale6.children[3].addEventListener('focusin', () => {speak('Rendement 42% voor alle oude centrales');});
-Centrale6.children[4].addEventListener('focusin', () => {speak('4.0 Megaton uitstoot per jaar');});
-
-Centrale7.children[0].addEventListener('focusin', () => {speak('Kolencentrale Essent');});
-Centrale7.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1994');});
-Centrale7.children[2].addEventListener('focusin', () => {speak('Vermogen 600 Mega Watt');});
-Centrale7.children[3].addEventListener('focusin', () => {speak('3.6 Megaton uitstoot per jaar');});
-
-sub2.children[0].addEventListener('focusin', () => {speak('Kop, Totaal centrales jaren 90');});
-sub2.children[1].addEventListener('focusin', () => {speak('Kop, leeg');});
-sub2.children[2].addEventListener('focusin', () => {speak('Kop, Totaal vermogen is 1230');});
-sub2.children[3].addEventListener('focusin', () => {speak('Kop, Leeg');});
-sub2.children[4].addEventListener('focusin', () => {speak('Kop, Totale uitstoot 7.6 ton per jaar');});
-
-Centrale8.children[0].addEventListener('focusin', () => {speak('Kolencentrale Engie in Maasvlakte');});
-Centrale8.children[1].addEventListener('focusin', () => {speak('Bouwjaar 2015');});
-Centrale8.children[2].addEventListener('focusin', () => {speak('Vermogen 800 Mega Watt');});
-Centrale8.children[3].addEventListener('focusin', () => {speak('Rendement 46% voor alle oude centrales');});
-Centrale8.children[4].addEventListener('focusin', () => {speak('3.2 Megaton uitstoot per jaar');});
-
-Centrale9.children[0].addEventListener('focusin', () => {speak('Kolencentrale Uniper in Maasvlakte');});
-Centrale9.children[1].addEventListener('focusin', () => {speak('Bouwjaar 2016');});
-Centrale9.children[2].addEventListener('focusin', () => {speak('Vermogen 1070 Mega Watt');});
-Centrale9.children[3].addEventListener('focusin', () => {speak('4.7 Megaton uitstoot per jaar');});
-
-Centrale10.children[0].addEventListener('focusin', () => {speak('Kolencentrale Essent in Eemshaven');});
-Centrale10.children[1].addEventListener('focusin', () => {speak('Bouwjaar 2015');});
-Centrale10.children[2].addEventListener('focusin', () => {speak('Vermogen 1560 Mega Watt');});
-Centrale10.children[3].addEventListener('focusin', () => {speak('8.3 Megaton uitstoot per jaar');});
-
-sub3.children[0].addEventListener('focusin', () => {speak('Kop, Totaal nieuwe centrales');});
-sub3.children[1].addEventListener('focusin', () => {speak('Kop, leeg');});
-sub3.children[2].addEventListener('focusin', () => {speak('Kop, Totaal vermogen is 3430');});
-sub3.children[3].addEventListener('focusin', () => {speak('Kop, Leeg');});
-sub3.children[4].addEventListener('focusin', () => {speak('Kop, Totale uitstoot 16.2 ton per jaar');});
---
+++ docs/js/table.js
@@ -0,0 +1,92 @@
+//table
+let header = findElement('header');
+let Centrale1 = findElement('C1');
+    Centrale2 = findElement('C2');
+    Centrale3 = findElement('C3');
+    Centrale4 = findElement('C4');
+    Centrale5 = findElement('C5');
+    Centrale6 = findElement('C6');
+    Centrale7 = findElement('C7');
+    Centrale8 = findElement('C8');
+    Centrale9 = findElement('C9');
+    Centrale10 = findElement('C10');
+let sub1 = findElement('sub1');
+    sub2 = findElement('sub2');
+    sub3 = findElement('sub3');
+
+    header.children[0].addEventListener('focusin', () => {speak('Kop, Oude kolencentrales');});
+    header.children[1].addEventListener('focusin', () => {speak('Kop, Bouwjaar');});
+    header.children[2].addEventListener('focusin', () => {speak('Kop, Vermogen');});
+    header.children[3].addEventListener('focusin', () => {speak('Kop, rendement');});
+    header.children[4].addEventListener('focusin', () => {speak('Kop, C o 2 uitstoot');});
+    
+    Centrale1.children[0].addEventListener('focusin', () => {speak('Kolencentrale Delta in Borsele');});
+    Centrale1.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1988');});
+    Centrale1.children[2].addEventListener('focusin', () => {speak('Vermogen 406 Mega Watt');});
+    Centrale1.children[3].addEventListener('focusin', () => {speak('Rendement 38% voor alle oude centrales');});
+    Centrale1.children[4].addEventListener('focusin', () => {speak('2.1 Megaton uitstoot per jaar');});
+    
+    Centrale2.children[0].addEventListener('focusin', () => {speak('Kolencentrale Essent');});
+    Centrale2.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1981');});
+    Centrale2.children[2].addEventListener('focusin', () => {speak('Vermogen 645 Mega Watt');});
+    Centrale2.children[3].addEventListener('focusin', () => {speak('2.1 Megaton uitstoot per jaar');});
+    
+    Centrale3.children[0].addEventListener('focusin', () => {speak('Kolencentrale Engie in Gelderland');});
+    Centrale3.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1985');});
+    Centrale3.children[2].addEventListener('focusin', () => {speak('Vermogen 570 Mega Watt');});
+    Centrale3.children[3].addEventListener('focusin', () => {speak('2.8 Megaton uitstoot per jaar');});
+    
+    Centrale4.children[0].addEventListener('focusin', () => {speak('Kolencentrale Uniper in Maasvlakte');});
+    Centrale4.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1988');});
+    Centrale4.children[2].addEventListener('focusin', () => {speak('Vermogen 500 Mega Watt');});
+    Centrale4.children[3].addEventListener('focusin', () => {speak('3.0 Megaton uitstoot per jaar');});
+    
+    Centrale5.children[0].addEventListener('focusin', () => {speak('Kolencentrale Uniper in Maasvlakte');});
+    Centrale5.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1989');});
+    Centrale5.children[2].addEventListener('focusin', () => {speak('Vermogen 500 Mega Watt');});
+    Centrale5.children[3].addEventListener('focusin', () => {speak('3.0 Megaton uitstoot per jaar');});
+    
+    sub1.children[0].addEventListener('focusin', () => {speak('Kop, Totaal oude kolencentrales');});
+    sub1.children[1].addEventListener('focusin', () => {speak('Kop, leeg');});
+    sub1.children[2].addEventListener('focusin', () => {speak('Kop, Totaal vermogen is 2621 mega watt voor oude centrales');});
+    sub1.children[3].addEventListener('focusin', () => {speak('Kop, Leeg');});
+    sub1.children[4].addEventListener('focusin', () => {speak('Kop, Totale uitstoot 13.0 voor oude centrales');});
+    
+    Centrale6.children[0].addEventListener('focusin', () => {speak('Kolencentrale NUON');});
+    Centrale6.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1995');});
+    Centrale6.children[2].addEventListener('focusin', () => {speak('Vermogen 630 Mega Watt');});
+    Centrale6.children[3].addEventListener('focusin', () => {speak('Rendement 42% voor alle oude centrales');});
+    Centrale6.children[4].addEventListener('focusin', () => {speak('4.0 Megaton uitstoot per jaar');});
+    
+    Centrale7.children[0].addEventListener('focusin', () => {speak('Kolencentrale Essent');});
+    Centrale7.children[1].addEventListener('focusin', () => {speak('Bouwjaar 1994');});
+    Centrale7.children[2].addEventListener('focusin', () => {speak('Vermogen 600 Mega Watt');});
+    Centrale7.children[3].addEventListener('focusin', () => {speak('3.6 Megaton uitstoot per jaar');});
+    
+    sub2.children[0].addEventListener('focusin', () => {speak('Kop, Totaal centrales jaren 90');});
+    sub2.children[1].addEventListener('focusin', () => {speak('Kop, leeg');});
+    sub2.children[2].addEventListener('focusin', () => {speak('Kop, Totaal vermogen is 1230 mega watt');});
+    sub2.children[3].addEventListener('focusin', () => {speak('Kop, Leeg');});
+    sub2.children[4].addEventListener('focusin', () => {speak('Kop, Totale uitstoot 7.6 ton per jaar');});
+    
+    Centrale8.children[0].addEventListener('focusin', () => {speak('Kolencentrale Engie in Maasvlakte');});
+    Centrale8.children[1].addEventListener('focusin', () => {speak('Bouwjaar 2015');});
+    Centrale8.children[2].addEventListener('focusin', () => {speak('Vermogen 800 Mega Watt');});
+    Centrale8.children[3].addEventListener('focusin', () => {speak('Rendement 46% voor alle oude centrales');});
+    Centrale8.children[4].addEventListener('focusin', () => {speak('3.2 Megaton uitstoot per jaar');});
+    
+    Centrale9.children[0].addEventListener('focusin', () => {speak('Kolencentrale Uniper in Maasvlakte');});
+    Centrale9.children[1].addEventListener('focusin', () => {speak('Bouwjaar 2016');});
+    Centrale9.children[2].addEventListener('focusin', () => {speak('Vermogen 1070 Mega Watt');});
+    Centrale9.children[3].addEventListener('focusin', () => {speak('4.7 Megaton uitstoot per jaar');});
+    
+    Centrale10.children[0].addEventListener('focusin', () => {speak('Kolencentrale Essent in Eemshaven');});
+    Centrale10.children[1].addEventListener('focusin', () => {speak('Bouwjaar 2015');});
+    Centrale10.children[2].addEventListener('focusin', () => {speak('Vermogen 1560 Mega Watt');});
+    Centrale10.children[3].addEventListener('focusin', () => {speak('8.3 Megaton uitstoot per jaar');});
+    
+    sub3.children[0].addEventListener('focusin', () => {speak('Kop, Totaal nieuwe centrales');});
+    sub3.children[1].addEventListener('focusin', () => {speak('Kop, leeg');});
+    sub3.children[2].addEventListener('focusin', () => {speak('Kop, Totaal vermogen is 3430');});
+    sub3.children[3].addEventListener('focusin', () => {speak('Kop, Leeg');});
+    sub3.children[4].addEventListener('focusin', () => {speak('Kop, Totale uitstoot 16.2 ton per jaar');});
